992a40f7869318798b9722654ed2063e3795ccb0
LEEDAESOL <eothf97@gmail.com>
2024-04-01 13:18:29 +0900

    Krazy_Karts Start

---
+++ Krazy_Karts/Source/Krazy_Karts/NotUseThis.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class KRAZY_KARTS_API NotUseThis
+{
+public:
+	NotUseThis();
+	~NotUseThis();
+};
